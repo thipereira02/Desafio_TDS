9d0d52b9f56cc93de991d36261ad7061bf692f5d
Thiago <thiago.spereira02@gmail.com>
2023-07-28 23:11:58 -0300

    initial files

---
+++ src/test/java/com/thiago/urlshortener/UrlShortenerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
