635144ae3f7311d62a78fac757a0b33cb422540f
Thiago <thiago.spereira02@gmail.com>
2023-07-29 20:46:38 -0300

    fix: updating validation regex

--- src/main/java/com/thiago/urlshortener/utils/UrlValidator.java
+++ src/main/java/com/thiago/urlshortener/utils/UrlValidator.java
@@ -1,9 +1,12 @@
 package com.thiago.urlshortener.utils;
 
+import java.util.regex.Pattern;
+
 public class UrlValidator {
-    private static final String URL_REGEX = "^(http|https)://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final String URL_REGEX = "^(http|https)://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}(/.*)?$";
+    private static final Pattern URL_PATTERN = Pattern.compile(URL_REGEX);
 
     public static boolean isValid(String url) {
-        return url.matches(URL_REGEX);
+        return URL_PATTERN.matcher(url).matches();
     }
 }
