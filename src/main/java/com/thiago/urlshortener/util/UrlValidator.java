20b87670148ea557eb0b2142c0e72a73beb77147
Thiago <thiago.spereira02@gmail.com>
2023-07-29 17:58:51 -0300

    chore: updating UrlValidator regex for improved URL validation

--- src/main/java/com/thiago/urlshortener/util/UrlValidator.java
+++ src/main/java/com/thiago/urlshortener/util/UrlValidator.java
@@ -1,7 +1,7 @@
 package com.thiago.urlshortener.util;
 
 public class UrlValidator {
-    private static final String URL_REGEX = "^((https?|ftp)://|(www|ftp)\\.)?[a-z0-9-]+(\\.[a-z0-9-]+)+([/?].*)?$";
+    private static final String URL_REGEX = "^(http|https)://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
     public static boolean isValid(String url) {
         return url.matches(URL_REGEX);
