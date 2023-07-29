fb025e22ade1ea7e2b6b9374904a19924ee83d98
Thiago <thiago.spereira02@gmail.com>
2023-07-29 20:47:32 -0300

    chore: adding unit test to UrlValidator

--- src/test/java/com/thiago/urlshortener/units/utils/UrlValidatorTest.java
+++ src/test/java/com/thiago/urlshortener/units/utils/UrlValidatorTest.java
@@ -1,5 +1,30 @@
 package com.thiago.urlshortener.units.utils;
 
+import com.thiago.urlshortener.utils.UrlValidator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class UrlValidatorTest {
     
+    @Test
+    public void testValidateUrl() {
+        assertTrue(UrlValidator.isValid("http://www.example.com"));
+        assertTrue(UrlValidator.isValid("https://example.com"));
+    }
+
+    @Test
+    public void testInvalidUrl() {
+        assertFalse(UrlValidator.isValid("www.example"));
+        assertFalse(UrlValidator.isValid("example.com"));
+        assertFalse(UrlValidator.isValid("example"));
+        assertFalse(UrlValidator.isValid("http://"));
+        assertFalse(UrlValidator.isValid("http://www"));
+        assertFalse(UrlValidator.isValid("http://www."));
+        assertFalse(UrlValidator.isValid("http://www.example."));
+        assertFalse(UrlValidator.isValid("http://www.example.c"));
+        assertFalse(UrlValidator.isValid("http://www.example.com@invalid"));
+    }
 }
