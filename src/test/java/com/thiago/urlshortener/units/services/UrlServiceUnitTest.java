aca3c192464b445492481aa186c301c03ffe425f
Thiago <thiago.spereira02@gmail.com>
2023-07-30 13:31:32 -0300

    feat: adding unit test for methods getOriginalUrl and updateUrlStatistics

--- src/test/java/com/thiago/urlshortener/units/services/UrlServiceUnitTest.java
+++ src/test/java/com/thiago/urlshortener/units/services/UrlServiceUnitTest.java
@@ -45,6 +45,41 @@ public class UrlServiceUnitTest {
         String result = urlService.shortenUrl(originalUrl);
 
         assertEquals(shortUrl, result);
+    }
 
+    @Test
+    public void testGetOriginalUrl_whenUrlExists() {
+        String shortUrl = "http://encurte.com/abc12";
+        UrlEntity urlEntity = MockUtils.mockUrlEntity(shortUrl);
+
+        when(urlRepository.findByShortUrl(shortUrl)).thenReturn(urlEntity);
+
+        String result = urlService.getOriginalUrl(shortUrl);
+
+        assertEquals(urlEntity.getOriginalUrl(), result);
+    }
+
+    @Test
+    public void testGetOriginalUrl_whenUrlDoesNotExist() {
+        String shortUrl = "http://encurte.com/abc12";
+
+        when(urlRepository.findByShortUrl(shortUrl)).thenReturn(null);
+
+        String result = urlService.getOriginalUrl(shortUrl);
+
+        assertEquals(null, result);
+    }
+
+    @Test
+    public void testUpdateUrlStatistics_whenUrlExists() {
+        String shortUrl = "http://encurte.com/abc12";
+        UrlEntity urlEntity = MockUtils.mockUrlEntity(shortUrl);
+
+        when(urlRepository.findByShortUrl(shortUrl)).thenReturn(urlEntity);
+        when(urlRepository.save(any(UrlEntity.class))).thenReturn(urlEntity);
+
+        urlService.updateUrlStatistics(shortUrl);
+
+        assertEquals(1, urlEntity.getClicks());
     }
 }
